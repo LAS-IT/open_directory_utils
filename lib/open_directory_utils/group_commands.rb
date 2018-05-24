4b00adc859a27d71817b632ccd9f232ec999b206
Bill Tihen <btihen@gmail.com>
2018-05-24 13:49:49 +0200

    looked up all dscl and other commands for needed methods

--- lib/open_directory_utils/group_commands.rb
+++ lib/open_directory_utils/group_commands.rb
@@ -1,22 +1,35 @@
 module OpenDirectoryUtils
+  # http://krypted.com/mac-os-x/create-groups-using-dscl/
+  # https://apple.stackexchange.com/questions/307173/creating-a-group-via-users-groups-in-command-line?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
   module GroupCommands
 
+    # dscl . read /Groups/ladmins
     def group_get_info
     end
 
     def group_exists?
     end
 
+    # create group     -- dscl . -create /Groups/ladmins
+    # add group passwd -- dscl . -create /Groups/ladmins passwd “*”
+    # add group name   -- dscl . -create /Groups/ladmins RealName “Local Admins”
+    # group ID number  -- dscl . -create /Groups/ladmins gid 400
+    # group id number  -- dscl . -create /Groups/GROUP PrimaryGroupID GID
     def group_create
     end
 
+    # dscl . -delete /Groups/yourGroupName
+    # https://tutorialforlinux.com/2011/09/15/delete-users-and-groups-from-terminal/
     def group_delete
     end
 
+    # add 1st user   -- dscl . create /Groups/ladmins GroupMembership localadmin
+    # add more users -- dscl . append /Groups/ladmins GroupMembership 2ndlocaladmin
     def group_add_user
     end
 
     # /usr/bin/dscl -u diradmin -P A-B1g-S3cret /LDAPv3/127.0.0.1/ -delete /Groups/$VALUE GroupMembership $UID_USERNAME
+    # dseditgroup -o edit -d $Username -t user $GroupName
     def group_remove_user
     end
 
--- lib/open_directory_utils/user_commands.rb
+++ lib/open_directory_utils/user_commands.rb
@@ -1,15 +1,43 @@
 module OpenDirectoryUtils
+  # https://developer.apple.com/legacy/library/documentation/Darwin/Reference/ManPages/man1/dscl.1.html
+  # https://superuser.com/questions/592921/mac-osx-users-vs-dscl-command-to-list-user/621055?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
   module UserCommands
 
+    # get all usernames -- dscl . -list /Users
+    # get all user details -- dscl . -readall /Users
     def user_exists?
     end
 
+    # get user record -- dscl . -read /Users/<username>
+    # get user value  -- dscl . -read /Users/<username> <key>
+    # search od user  -- dscl . -search /Users RealName "Andrew Garrett"
+    # return as xml   -- dscl -plist . -search /Users RealName "Andrew Garrett"
     def user_get_info
     end
 
+    # https://images.apple.com/server/docs/Command_Line.pdf
+    # https://serverfault.com/questions/20702/how-do-i-create-user-accounts-from-the-terminal-in-mac-os-x-10-5?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    # https://superuser.com/questions/1154564/how-to-create-a-user-from-the-macos-command-line
+    # There are a few steps to create a user account from the command line. The good news is that you're using the right tool, dscl. What you're missing are the separate components that comprise a user account. You have to create these manually.
+    # sudo dscl . -create /Users/someuser
+    # sudo dscl . -create /Users/someuser UserShell /bin/bash
+    # sudo dscl . -create /Users/someuser RealName "Lucius Q. User"
+    # sudo dscl . -create /Users/someuser UniqueID "1010"  #use something not already in use
+    # sudo dscl . -create /Users/someuser PrimaryGroupID 80
+    # sudo dscl . -create /Users/someuser NFSHomeDirectory /Users/soemuser
+    #
+    # You can then use passwd to change the user's password, or use:
+    # sudo dscl . -passwd /Users/someuser password
+
+    # You'll also have to create the user's home directory and change ownership so the user can access it. And be sure that the UniqueID is, in fact, unique.
+    #
+    # This line will add the user to the administrator's group:
+    # sudo dscl . -append /Groups/admin GroupMembership someuser
     def user_create
     end
 
+    # add 1st user   -- dscl . create /Groups/ladmins GroupMembership localadmin
+    # add more users -- dscl . append /Groups/ladmins GroupMembership 2ndlocaladmin
     def user_add_to_group
     end
 
@@ -17,6 +45,8 @@ module OpenDirectoryUtils
     def user_remove_from_group
     end
 
+    # dscl . -delete /Users/yourUserName
+    # https://tutorialforlinux.com/2011/09/15/delete-users-and-groups-from-terminal/
     def user_delete
     end
 
