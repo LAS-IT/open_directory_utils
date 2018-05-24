5e7a332c84b35b047f93baa240a323c1e9fa0888
Bill Tihen <btihen@gmail.com>
2018-05-24 09:26:14 +0200

    remove eq_command path

--- lib/open_directory_utils/connection.rb
+++ lib/open_directory_utils/connection.rb
@@ -9,13 +9,11 @@ module OpenDirectoryUtils
       config = defaults.merge(params)
       @hostname    = config[:hostname]
       @username    = config[:username]
-      @servicename = config[:servicename]
       @eqcmd_path  = config[:eqcmd_path]
       @ssh_options = config[:ssh_options]
 
       raise ArgumentError, 'hostname missing'    if hostname.nil? or hostname.empty?
       raise ArgumentError, 'username missing'    if username.nil? or username.empty?
-      raise ArgumentError, 'servicename missing' if servicename.nil? or servicename.empty?
     end
 
     def run(command:, attributes:)
@@ -34,7 +32,6 @@ module OpenDirectoryUtils
     def defaults
       { hostname: ENV['OD_HOSTNAME'],
         username: ENV['OD_USERNAME'],
-        servicename: ENV['OD_SERVICENAME'],
         ssh_options: (eval(ENV['OD_SSH_OPTIONS'].to_s) || {}) }
     end
 
