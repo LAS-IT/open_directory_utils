306c36c8eed54bd892141ef69e883a50ebf512c4
Bill Tihen <btihen@gmail.com>
2018-06-08 19:20:59 +0200

    switching groups to dseditgroup

--- lib/open_directory_utils/commands_base.rb
+++ lib/open_directory_utils/commands_base.rb
@@ -8,6 +8,16 @@ module OpenDirectoryUtils
 
     include OpenDirectoryUtils::CleanCheck
 
+    # builds the pwpolicy commands (after checking parameters)
+    # @attribs [Hash] - required - :record_name (the resource/user/group to affect), attribute: (resource attribute to change), value: (value to add to attribute)
+    # @dir_info [Hash] - usually configured in the connection initializer and then passed to pwpolicy to build command correctly
+    def pwpolicy(params, dir_info)
+      check_critical_attribute( params, :record_name )
+      cmd_params = tidy_attribs(params)
+
+      build_pwpolicy_command( cmd_params, dir_info )
+    end
+
     # builds the dscl command (after checking parameters)
     # @attribs [Hash] - required - :record_name (the resource to affect), :action (create, append, delete, passwd, etc), attribute: (resource attribute to change), value: (value to add to attribute)
     # @dir_info [Hash] - usually configured in the connection initializer and then passed to dscl to build command correctly
@@ -19,23 +29,26 @@ module OpenDirectoryUtils
       build_dscl_command( tidy_attribs, dir_info )
     end
 
-    # builds the pwpolicy commands (after checking parameters)
-    # @attribs [Hash] - required - :record_name (the resource/user/group to affect), attribute: (resource attribute to change), value: (value to add to attribute)
-    # @dir_info [Hash] - usually configured in the connection initializer and then passed to pwpolicy to build command correctly
-    def pwpolicy(params, dir_info)
-      check_critical_attribute( params, :record_name )
-      cmd_params = tidy_attribs(params)
-
-      build_pwpolicy_command( cmd_params, dir_info )
+    def dseditgroup(attribs, dir_info)
+      check_critical_attribute( attribs, :value )
+      check_critical_attribute( attribs, :operation )
+      if attribs[:operation].eql?('edit')
+        check_critical_attribute( attribs, :record_name )
+        check_critical_attribute( attribs, :action )
+        check_critical_attribute( attribs, :type )
+      end
+      tidy_attribs = tidy_attribs(attribs)
+      build_dseditgroup_command( tidy_attribs, dir_info )
     end
 
+    # /usr/bin/pwpolicy -a diradmin -p "BigSecret" -u username -setpolicy "isDisabled=0"
     def build_pwpolicy_command(params, dir_info)
-      # /usr/bin/pwpolicy -a diradmin -p "BigSecret" -u username -setpolicy "isDisabled=0"
-      ans  = "#{dir_info[:pwpol]}"
-      ans += " -a #{dir_info[:username]}"      unless dir_info[:username].nil? or
+      ans  = %Q[#{dir_info[:pwpol]}]
+      ans += %Q[ -a #{dir_info[:username]}]    unless dir_info[:username].nil? or
                                                       dir_info[:username].empty?
       ans += %Q[ -p "#{dir_info[:password]}"]  unless dir_info[:password].nil? or
                                                       dir_info[:password].empty?
+      ans += %Q[ -n #{dir_info[:data_path]}]
       ans += %Q[ -u #{params[:record_name]}]
       ans += %Q[ -#{params[:attribute]}]
       ans += %Q[ "#{params[:value]}"]          unless params[:value].nil? or
@@ -49,15 +62,15 @@ module OpenDirectoryUtils
       attribs[:record_name] = attribs[:record_name] || attribs[:recordname]
       # /usr/bin/dscl -u diradmin -P "BigSecret" /LDAPv3/127.0.0.1/ -append /Users/$UID_USERNAME apple-keyword "$VALUE"
       # "/usr/bin/dscl -plist -u #{od_username} -P #{od_password} #{od_dsclpath} -#{command} #{resource} #{params}"
-      ans  = "#{dir_info[:dscl]}"
+      ans  = %Q[#{dir_info[:dscl]}]
       unless attribs[:format].nil?
         ans += ' -plist'                           if attribs[:format].eql? 'plist' or
                                                       attribs[:format].eql? 'xml'
       end
-      ans += " -u #{dir_info[:username]}"      unless dir_info[:username].nil? or
+      ans += %Q[ -u #{dir_info[:username]}]    unless dir_info[:username].nil? or
                                                       dir_info[:username].empty? or
                                                       attribs[:action].eql? 'auth'
-      ans += %Q[ -P "#{dir_info[:password]}"]    unless dir_info[:password].nil? or
+      ans += %Q[ -P "#{dir_info[:password]}"]  unless dir_info[:password].nil? or
                                                       dir_info[:password].empty? or
                                                       attribs[:action].eql? 'auth'
       ans += " #{dir_info[:data_path]}"
@@ -71,6 +84,37 @@ module OpenDirectoryUtils
       ans += %Q[ "#{attribs[:value]}"]         unless attribs[:value].nil? or
                                                       attribs[:value].empty?
       return ans
+    end
+
+    # http://www.manpagez.com/man/8/dseditgroup/
+    # make a new group:
+    # dseditgroup -o create -n /LDAPv3/ldap.company.com -u dir_admin_user -P dir_admin_passwd \
+    #           -r "Real Group Name" -c "a comment" -k "keyword" groupname
+    # delete a new group:
+    # dseditgroup -o delete -n /LDAPv3/ldap.company.com -u dir_admin_user -P dir_admin_passwd groupname
+    # add a user to a group
+    # dseditgroup -o edit -n /LDAPv3/ldap.company.com -u dir_admin_user -P dir_admin_passwd -a username -t user groupname
+    # remove a user from a group
+    # dseditgroup -o edit -n /LDAPv3/ldap.company.com -u dir_admin_user -P dir_admin_passwd -d username -t user groupname
+    def build_dseditgroup_command( params, dir_info )
+      ans  = %Q[#{dir_info[:dsedit]}]
+      ans += %Q[ -o #{params[:operation]}]
+      ans += %Q[ -u #{dir_info[:username]}]    unless dir_info[:username].nil? or
+                                                      dir_info[:username].empty?
+      ans += %Q[ -P "#{dir_info[:password]}"]  unless dir_info[:password].nil? or
+                                                      dir_info[:password].empty?
+      ans += %Q[ -n #{dir_info[:data_path]}]
+      if params[:operation].eql?('create')
+        ans += %Q[ -r "#{params[:value]}"]         if params[:real_name].to_s.eql?('')
+        ans += %Q[ -r "#{params[:real_name]}"] unless params[:real_name].to_s.eql?('')
+        ans += %Q[ -k #{params[:keyword]}]     unless params[:keyword].to_s.eql?('')
+      end
+      if params[:operation].eql?('edit')
+        ans += %Q[ -a #{params[:record_name]}]     if params[:action].to_s.eql?('add')
+        ans += %Q[ -d #{params[:record_name]}]     if params[:action].to_s.eql?('delete')
+        ans += %Q[ -t #{params[:type]}]            # type can be user or group
+      end
+      ans += %Q[ #{params[:value]}]   # the group to be manipulated
     end
 
   end
--- spec/open_directory_utils/commands_base_spec.rb
+++ spec/open_directory_utils/commands_base_spec.rb
@@ -3,33 +3,33 @@ require "open_directory_utils/commands_base"
 
 RSpec.describe OpenDirectoryUtils::CommandsBase do
 
+  let(:base)     { Object.new.extend(OpenDirectoryUtils::CommandsBase) }
+  let(:srv_info) { {username: 'diradmin', password: 'TopSecret',
+                    data_path: '/LDAPv3/127.0.0.1/',
+                    dscl: '/usr/bin/dscl',
+                    pwpol: '/usr/bin/pwpolicy',
+                    dsedit: '/usr/bin/dseditgroup'} }
+
   context "dscl commands" do
-
-    let(:dscl)     { Object.new.extend(OpenDirectoryUtils::CommandsBase) }
-    let(:srv_info) { {username: 'diradmin', password: 'TopSecret',
-                      data_path: '/LDAPv3/127.0.0.1/',
-                      dscl: '/usr/bin/dscl',
-                      pwpol: '/usr/bin/pwpolicy'} }
-
     describe "errors with incorrect shortname" do
       it "empty hash" do
         attribs  = {}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: 'nil' invalid/)
       end
       it "nil" do
         attribs  = {record_name: nil}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: 'nil' invalid/)
       end
       it "'' (blank)" do
         attribs = {record_name: ''}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: '""' invalid/)
       end
       it "'with space' (no space allowed)" do
         attribs = {record_name: 'with space'}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: '"with space"' invalid/)
       end
     end
@@ -37,7 +37,7 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
     describe "errors with incorrect action" do
       it "nil" do
         attribs  = {record_name: 'valid', action: nil, scope: 'Users'}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /action: 'nil' invalid/)
       end
     end
@@ -45,7 +45,7 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
     describe "errors with incorrect scope" do
       it "'with space' (no space allowed)" do
         attribs  = {record_name: 'valid', action: 'valid', scope: 'with space'}
-        expect { dscl.send(:dscl, attribs, srv_info) }.
+        expect { base.send(:dscl, attribs, srv_info) }.
             to raise_error(ArgumentError, /scope: '"with space"' invalid/)
       end
     end
@@ -53,32 +53,32 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
     describe "build actions - fix extra spaces with shortname" do
       it "fixes shortname with trailing space" do
         attribs = {record_name: 'someone ', action: 'read', scope: 'Users'}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "fixes shortname with trailing spaces" do
         attribs = {record_name: 'someone  ', action: 'read', scope: 'Users'}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "fixes shortname with leading space" do
         attribs = { record_name: ' someone', action: 'read', scope: 'Users',
                     attribute: nil, value: nil}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "fixes shortname with trailing spaces" do
         attribs = {record_name: '  someone', action: 'read', scope: 'Users'}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "fixes shortname with trailing and leading spaces" do
         attribs = {record_name: '  someone  ', action: 'read', scope: 'Users'}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
@@ -88,41 +88,41 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
       it "read base user (nil attributes, return text)" do
         attribs = { record_name: ' someone', action: 'read', scope: 'Users',
                     attribute: nil, value: nil, format: nil }
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "read base user (return xml)" do
         attribs = { record_name: ' someone', action: 'read', scope: 'Users',
                     format: 'xml' }
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -plist -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "read base user (return text)" do
         attribs = { record_name: ' someone', action: 'read', scope: 'Users',
                     format: 'text' }
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -read /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "create base user (no other attributes, return text)" do
         attribs = { record_name: ' someone', action: 'create', scope: 'Users'}
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -create /Users/someone'
         expect( answer ).to eq( correct )
       end
       it "create user first keyword (return text)" do
         attribs = { record_name: ' someone', action: 'create', scope: 'Users',
                     attribute: 'keyword', value: 'student', format: 'text' }
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -create /Users/someone keyword "student"'
         expect( answer ).to eq( correct )
       end
       it "append user second keyword (return xml)" do
         attribs = { record_name: ' someone', action: 'append', scope: 'Users',
                     attribute: 'keyword', value: 'departed', format: 'plist' }
-        answer  = dscl.send(:dscl, attribs, srv_info)
+        answer  = base.send(:dscl, attribs, srv_info)
         correct = '/usr/bin/dscl -plist -u diradmin -P "TopSecret" /LDAPv3/127.0.0.1/ -append /Users/someone keyword "departed"'
         expect( answer ).to eq( correct )
       end
@@ -136,18 +136,18 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
                       dscl: '/usr/bin/dscl',
                       pwpol: '/usr/bin/pwpolicy'} }
 
-    describe "pwpolicy self-built commands" do
+    describe "pwpolicy built by hand" do
       it "user_enable_login" do
         attribs = {record_name: 'someone', attribute: 'enableuser', value: nil}
-        answer  = policy.send(:pwpolicy, attribs, srv_info)
-        correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -u someone -enableuser'
+        answer  = base.send(:pwpolicy, attribs, srv_info)
+        correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -n /LDAPv3/127.0.0.1/ -u someone -enableuser'
         # correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -u someone -setpolicy "isDisabled=0"'
         expect( answer ).to eq( correct )
       end
       it "user_disable_login" do
         attribs = {record_name: 'someone', attribute: 'disableuser', value: nil}
-        answer  = policy.send(:pwpolicy, attribs, srv_info)
-        correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -u someone -disableuser'
+        answer  = base.send(:pwpolicy, attribs, srv_info)
+        correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -n /LDAPv3/127.0.0.1/ -u someone -disableuser'
         # correct = '/usr/bin/pwpolicy -a diradmin -p "TopSecret" -u someone -setpolicy "isDisabled=1"'
         expect( answer ).to eq( correct )
       end
@@ -156,20 +156,90 @@ RSpec.describe OpenDirectoryUtils::CommandsBase do
     describe ":check_record_name check errors with bad shortname attribute" do
       it "shortname is nil" do
         attribs  = {record_name: nil}
-        expect { policy.send(:pwpolicy, attribs, srv_info) }.
+        expect { base.send(:pwpolicy, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: 'nil' invalid/)
       end
       it "shortname is '' (blank)" do
         attribs = {record_name: ''}
-        expect { policy.send(:pwpolicy, attribs, srv_info) }.
+        expect { base.send(:pwpolicy, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: '""' invalid/)
       end
       it "shortname = 'with space' (no space allowed)" do
         attribs = {record_name: 'with space'}
-        expect { policy.send(:pwpolicy, attribs, srv_info) }.
+        expect { base.send(:pwpolicy, attribs, srv_info) }.
             to raise_error(ArgumentError, /record_name: '"with space"' invalid/)
       end
     end
-
   end
+
+  context "test dseditgroup" do
+    describe "dseditgroup build by hand" do
+      it "all good data - to add a user" do
+        attribs = { operation: 'edit', value: 'groupname',
+                    record_name: 'username', action: 'add', type: 'user'}
+        answer  = base.send(:dseditgroup, attribs, srv_info)
+        correct = '/usr/bin/dseditgroup -o edit -u diradmin -P "TopSecret" -n /LDAPv3/127.0.0.1/ -a username -t user groupname'
+        expect( answer ).to eq( correct )
+      end
+      it "all good data - to remove a user" do
+        attribs = { operation: 'edit', value: 'groupname',
+                    record_name: 'username', action: 'delete', type: 'user'}
+        answer  = base.send(:dseditgroup, attribs, srv_info)
+        correct = '/usr/bin/dseditgroup -o edit -u diradmin -P "TopSecret" -n /LDAPv3/127.0.0.1/ -d username -t user groupname'
+        expect( answer ).to eq( correct )
+      end
+      it "all good data - create a group" do
+        attribs = { operation: 'create', value: 'groupname', real_name: 'Group Name'}
+        answer  = base.send(:dseditgroup, attribs, srv_info)
+        correct = '/usr/bin/dseditgroup -o create -u diradmin -P "TopSecret" -n /LDAPv3/127.0.0.1/ -r "Group Name" groupname'
+        expect( answer ).to eq( correct )
+      end
+      it "all good data - create a group - no realname" do
+        attribs = { operation: 'create', value: 'groupname'}
+        answer  = base.send(:dseditgroup, attribs, srv_info)
+        correct = '/usr/bin/dseditgroup -o create -u diradmin -P "TopSecret" -n /LDAPv3/127.0.0.1/ -r "groupname" groupname'
+        expect( answer ).to eq( correct )
+      end
+      it "all good data - delete a group" do
+        attribs = { operation: 'delete', value: 'groupname'}
+        answer  = base.send(:dseditgroup, attribs, srv_info)
+        correct = '/usr/bin/dseditgroup -o delete -u diradmin -P "TopSecret" -n /LDAPv3/127.0.0.1/ groupname'
+        expect( answer ).to eq( correct )
+      end
+    end
+
+    describe "errors adding user to a group" do
+      it "operation is nil" do
+        attribs = { operation: nil, value: 'groupname',
+                    record_name: 'username', action: 'add', type: 'user'}
+        expect { base.send(:dseditgroup, attribs, srv_info) }.
+            to raise_error(ArgumentError, /operation: 'nil' invalid/)
+      end
+      it "groupname is nil" do
+        attribs = { operation: 'edit', value: nil,
+                    record_name: 'username', action: 'add', type: 'user'}
+        expect { base.send(:dseditgroup, attribs, srv_info) }.
+            to raise_error(ArgumentError, /value: 'nil' invalid/)
+      end
+      it "edit - username is nil" do
+        attribs = { operation: 'edit', value: 'groupname',
+                    record_name: nil, action: 'add', type: 'user'}
+        expect { base.send(:dseditgroup, attribs, srv_info) }.
+            to raise_error(ArgumentError, /record_name: 'nil' invalid/)
+      end
+      it "edit - action is nil" do
+        attribs = { operation: 'edit', value: 'groupname',
+                    record_name: 'username', action: nil, type: 'user'}
+        expect { base.send(:dseditgroup, attribs, srv_info) }.
+            to raise_error(ArgumentError, /action: 'nil' invalid/)
+      end
+      it "edit - type is nil" do
+        attribs = { operation: 'edit', value: 'groupname',
+                    record_name: 'username', action: 'add', type: nil}
+        expect { base.send(:dseditgroup, attribs, srv_info) }.
+            to raise_error(ArgumentError, /type: 'nil' invalid/)
+      end
+    end
+  end
+
 end
